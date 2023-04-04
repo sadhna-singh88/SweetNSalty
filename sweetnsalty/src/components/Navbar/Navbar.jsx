42c1600ce1d556067e0fe9281c0149b158d6057f
Sadhna singh <sadhna.singh.work@gmail.com>
2023-04-04 21:35:26 +1200

    Navbar component completed

--- sweetnsalty/src/components/Navbar/Navbar.jsx
+++ sweetnsalty/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { MdOutlineRestaurantMenu } from 'react-icons/md'
 import images from '../../constants/images'
 import './Navbar.css'
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -39,33 +40,38 @@ const Navbar = () => {
         </a>
       </div>
       <div className="app__navbar-smallscreen">
-        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}} />
+        <GiHamburgerMenu
+          color="#fff"
+          fontSize={27}
+          onClick={() => setToggleMenu(true)}
+        />
+        {toggleMenu && (
+          <div className="app__navbar-samllscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu
+              fontSize={27}
+              className="overlay_close"
+              onClick={() => setToggleMenu(false)}
+            />
 
-        <div className="app__navbar-samllscreen_overlay flex__center slide-bottom">
-          <MdOutlineRestaurantMenu
-            fontSize={27}
-            className="overlay_close"
-            onClick={() => {}}
-          />
-
-          <ul className="app__navbar-samllscreen-links">
-            <li className="p__opensans">
-              <a href="#home">Home</a>
-            </li>
-            <li className="p__opensans">
-              <a href="#about">About</a>
-            </li>
-            <li className="p__opensans">
-              <a href="#menu">Menu</a>
-            </li>
-            <li className="p__opensans">
-              <a href="#awards">Awards</a>
-            </li>
-            <li className="p__opensans">
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
-        </div>
+            <ul className="app__navbar-samllscreen_links">
+              <li className="p__opensans">
+                <a href="#home">Home</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#about">About</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#menu">Menu</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#awards">Awards</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </nav>
   )
