8b690f4ea2019f1d282ed71094fa9c56d6558c22
Sadhna singh <sadhna.singh.work@gmail.com>
2023-04-05 13:39:50 +1200

    created SubHeading component and added some images

--- sweetnsalty/src/constants/images.js
+++ sweetnsalty/src/constants/images.js
@@ -9,7 +9,7 @@ import knife from '../assets/knife.png'
 import logo from '../assets/logo.png'
 import menu from '../assets/menu.png'
 import overlaybg from '../assets/overlaybg.png'
-//import spoon from '../assets/spoon.svg'
+import spoon from '../assets/spoon.svg'
 import welcome from '../assets/welcome.png'
 import findus from '../assets/findus.png'
 import laurels from '../assets/laurels.png'
@@ -33,7 +33,7 @@ export default {
   logo,
   menu,
   overlaybg,
-  //spoon,
+  spoon,
   welcome,
   findus,
   laurels,
